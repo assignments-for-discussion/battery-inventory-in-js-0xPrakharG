da8a784832d7c10e68101d605f81f1af44e13477
Sudeep Prasad <sudeep.rp@numocity.com>
2022-01-01 21:00:48 +0530

    tweak-1

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function countBatteriesByUsage(cycles) {
 function testBucketingByNumberOfCycles() {
   console.log('Counting batteries by usage cycles...')
   counts = countBatteriesByUsage([100, 300, 500, 600, 900, 1000]);
-  assert(counts["lowCount"] == 1);
+  assert(counts["lowCount"] == 2);
   assert(counts["mediumCount"] == 3);
-  assert(counts["highCount"] == 2);
+  assert(counts["highCount"] == 1);
   console.log("Done counting :)");
 }
 
