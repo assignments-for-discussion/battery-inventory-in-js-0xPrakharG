16446076bb7d770ad0628602f453b44d891fd9e8
Sudeep Prasad <sudeep.rp@numocity.com>
2023-09-07 13:59:01 +0530

    SoH

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const assert = require('assert');
 
-function countBatteriesByUsage(cycles) {
+function countBatteriesByHealth(presentCapacities) {
   return {
-    lowCount: 0,
-    mediumCount: 0,
-    highCount: 0
+    healthy: 0,
+    exchange: 0,
+    failed: 0
   };
 }
 
-function testBucketingByNumberOfCycles() {
-  console.log('Counting batteries by usage cycles...')
-  counts = countBatteriesByUsage([100, 300, 500, 600, 900, 1000]);
-  assert(counts["lowCount"] == 2);
-  assert(counts["mediumCount"] == 3);
-  assert(counts["highCount"] == 1);
+function testBucketingByHealth() {
+  console.log('Counting batteries by SoH...');
+  const presentCapacities = [115, 118, 80, 95, 91, 77];
+  counts = countBatteriesByHealth(presentCapacities);
+  assert(counts["healthy"] == 2);
+  assert(counts["exchange"] == 3);
+  assert(counts["failed"] == 1);
   console.log("Done counting :)");
 }
 
-testBucketingByNumberOfCycles();
+testBucketingByHealth();
